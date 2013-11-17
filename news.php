066856ba47acef720952dd20c0ad59c99370bf5a
makaius <makaius89@gmail.com>
2013-11-17 21:11:49 +0100

    News finita

--- news.php
+++ news.php
@@ -29,9 +29,13 @@ $notizia= $newsDB->fetchByRow($news-1);
     <body>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
         <div class="scroll_box">
-            <div class="article_title"><?php echo $notizia->Titolo; ?></div>
+            
+            <div class="article_title"><?php 
+            echo $notizia->Titolo; ?></div>
             <div class="article_content">
-                <div class="article_date"><?php echo $notizia->DataIns; ?></div>
+                <div class="article_date"><?php 
+            $newDate = date("d.m.Y", strtotime($notizia->DataIns));
+            echo $newDate; ?></div>
                 <div class="article_image_box"><img class="article_image" src="<?php echo substr($notizia->Foto,3); ?>" /></div>
                 <div class="horiz_bar"></div>
                 <div class="article_text">
@@ -39,18 +43,7 @@ $notizia= $newsDB->fetchByRow($news-1);
                      <?php echo $notizia->Testo; ?>
                     </p>
                 </div>
-                <div class="social_networks_links">
-                    <a href="https://facebook.com" target="_blank">
-                        <div class="sn_link">
-                            <img class="link_mini_logo" alt="facebook_logo" src="images/links/fb_mini.png" />
-                        </div>
-                    </a>
-                    <a href="http://twitter.com" target="_blank">
-                        <div class="sn_link">
-                            <img class="link_mini_logo" alt="twitter_logo" src="images/links/twitter_mini.png" />
-                        </div>
-                    </a>
-                </div>
+                
                 <div id="news_navigation">
                     <div class="news_navigation_cell">
                          <?php
