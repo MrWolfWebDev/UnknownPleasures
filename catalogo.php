dd5fac177dd2c008d2d28e74852da8cf68d0963e
Andrea Valenza <avalenza89@gmail.com>
2013-11-20 14:39:32 +0100

    Lista Brani centrata

--- catalogo.php
+++ catalogo.php
@@ -12,21 +12,21 @@ $songDB = new TableSong();
     <body>
         <div class="scroll_box">
             <div class="logo"><img src="images/finder_logo.png" /></div>
-            
-                <div class="search_zone">
-                    <input id="lens" type="image" src="images/lens.png" name="submit" alt="Cerca"><input id="search_text" onclick="clickclear(this, 'cerca..');" onblur="clickrecall(this, 'cerca..');" onkeyup="Search();" type="text" value="cerca..">
-                </div>
-            
+
+            <div class="search_zone">
+                <input id="lens" type="image" src="images/lens.png" name="submit" alt="Cerca"><input id="search_text" onclick="clickclear( this, 'cerca..' );" onblur="clickrecall( this, 'cerca..' );" onkeyup="Search();" type="text" value="cerca..">
+            </div>
+
             <div class="horiz_bar"></div>
             <div class="catalogo_text" id="catalogo_text">
-                <table style="width: 540px;">
-                  <?php
-                $result = $songDB->fetchAll();
+                <table style="width: 540px; margin: 0 auto;">
+                    <?php
+                    $result = $songDB->fetchAll();
 
-                foreach ( $result as $song ) {
-                    echo "<tr><td>", $song->Titolo, "</td><td><a href=" . substr( $song->Pdf, 3 ) . " target='_blank'><img src='images/pdf.png' height='30px' width='50px'></a></td><td><a href=" . $song->ITunes . " target='_blank'><img src='images/nota.png' alt='iTunes' height='30px' width='30px'></a></td></tr>";
-                }
-                ?>
+                    foreach ( $result as $song ) {
+                        echo "<tr><td>", $song->Titolo, "</td><td><a href=" . substr( $song->Pdf, 3 ) . " target='_blank'><img src='images/pdf.png' height='30px' width='50px'></a></td><td><a href=" . $song->ITunes . " target='_blank'><img src='images/nota.png' alt='iTunes' height='30px' width='30px'></a></td></tr>";
+                    }
+                    ?>
                 </table>
             </div>
         </div>
